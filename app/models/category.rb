62cf9ab317a72e6d7ca58b077c1f89f3c584fd89
takalivian <61145524+takalivian@users.noreply.github.com>
2020-05-22 17:22:25 +0900

    category post associate

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+
+  has_many :posts
+  has_ancestry
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
 
   belongs_to :user
+  belongs_to :category
   has_many :images
   has_many :comments
 
