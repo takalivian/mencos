20544c63611bfe23dda76d67fa2e7d32de5d06da
takalivian <61145524+takalivian@users.noreply.github.com>
2020-05-22 16:17:46 +0900

    category reference

---
+++ db/migrate/20200522071423_add_references_to_posts.rb
@@ -0,0 +1,7 @@
+class AddReferencesToPosts < ActiveRecord::Migration[5.2]
+  def change
+
+    add_reference :posts, :category, foreign_key: true
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_063741) do
+ActiveRecord::Schema.define(version: 2020_05_22_071423) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "ancestry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "url"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_images_on_post_id"
+  end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -19,6 +34,8 @@ ActiveRecord::Schema.define(version: 2020_05_22_063741) do
     t.text "remark"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_posts_on_category_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -41,4 +58,6 @@ ActiveRecord::Schema.define(version: 2020_05_22_063741) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "images", "posts"
+  add_foreign_key "posts", "categories"
 end
