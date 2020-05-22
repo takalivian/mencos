b7a6c5c9c27eeb013ee37222c1488d48c1050984
takalivian <61145524+takalivian@users.noreply.github.com>
2020-05-22 15:52:41 +0900

    image column

--- db/migrate/20200522065013_create_images.rb
+++ db/migrate/20200522065013_create_images.rb
@@ -2,6 +2,9 @@ class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
 
+      t.string     :url
+      t.references :post, foreign_key: true
+
       t.timestamps
     end
   end
