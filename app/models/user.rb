2350bc3a3649af9b5d5a737dfdefa15a27b83ca9
takalivian <61145524+takalivian@users.noreply.github.com>
2020-05-26 16:21:11 +0900

    sns-user associate

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,23 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+  has_many :sns_credentials
   has_many :posts
   has_many :comments
 
   validates :name, presence: true, uniqueness: true
   
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = sns.user || User.where(email: auth.info.email).first_or_initialize(
+      name: auth.info.name,
+        email: auth.info.email
+    )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
+  end
 end
