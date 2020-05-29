4c7eaf6125931731a1427506dc81988e5b0f1ae4
takalivian <61145524+takalivian@users.noreply.github.com>
2020-05-29 13:31:43 +0900

    update back

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    if current_user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
