79a16dfdf1a4c7a6475354016cdf7feea484263f
takalivian <61145524+takalivian@users.noreply.github.com>
2020-05-31 01:45:03 +0900

    root setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'posts/index'
   devise_for :users, controllers: {
   omniauth_callbacks: 'users/omniauth_callbacks',
   registrations: 'users/registrations'
   }
   root to:"posts#index"
-  resources :users
+  resources :posts, only: [:index, :new, :show]
+  resources :users, only: [:show, :edit, :update]
 end
