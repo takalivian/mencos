db73f84efb86ce7f899b1754af5cdaa71121c4a6
takalivian <61145524+takalivian@users.noreply.github.com>
2020-05-31 22:39:10 +0900

    params,new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,24 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+    @post.images.new
+  end
+
+  def create
+    @ppst = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   
   def show
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :brand, :shop, :price, :remark, images_attributes: [:url])
   end
 
 end
