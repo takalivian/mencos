88a82671e0927e3137fffc2f237cf784338e2c38
takalivian <61145524+takalivian@users.noreply.github.com>
2020-06-04 18:05:01 +0900

    image dependent: :destroy

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
 
   belongs_to :user
   belongs_to :category, optional: true
-  has_many :images
+  has_many :images, dependent: :destroy
   has_many :comments
 
   accepts_nested_attributes_for :images, allow_destroy: true
