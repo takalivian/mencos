a5ac6caa4c3fad92cba3193f24cb2c523bcc9c53
takalivian <61145524+takalivian@users.noreply.github.com>
2020-06-06 00:57:09 +0900

    relation user-follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
   has_many :sns_credentials
   has_many :posts
   has_many :comments
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
 
   mount_uploader :image, ImageUploader
 
@@ -22,6 +26,22 @@ class User < ApplicationRecord
       sns.save
     end
     { user: user, sns: sns }
+  end
+
+  # フォロー
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
   end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
