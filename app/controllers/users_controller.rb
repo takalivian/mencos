ae030ab6c48e06298767bd18606b9429d3ced9e0
takalivian <61145524+takalivian@users.noreply.github.com>
2020-06-06 01:36:40 +0900

    follow-btn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def following
+    @user = User.find(params[:id])
+    @users = @user.following
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers
+  end
+
   private
 
   def user_params
