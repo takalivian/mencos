fa1b3353cb1c0eebcb8b6f79bca6a9920dfddfa0
takalivian <61145524+takalivian@users.noreply.github.com>
2020-06-06 00:55:21 +0900

    relationships setting

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,5 @@ Rails.application.routes.draw do
     resources :comments, only: :create
   end
   resources :users, only: [:show, :edit, :update]
+  resources :relationships, only: [:create, :destroy]
 end
