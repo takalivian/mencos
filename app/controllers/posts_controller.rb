b6635d4e56856ce6eb6f43928493cdd46e9fd97e
takalivian <61145524+takalivian@users.noreply.github.com>
2020-06-05 16:07:46 +0900

    コメントアウト削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,22 +7,18 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
     @post.images.new
-    #セレクトボックスの初期値設定
     @category_parent_array = ["---"]
-    #データベースから、親カテゴリーのみ抽出し、配列化
     Category.where(ancestry: nil).each do |parent|
       @category_parent_array << parent.name
     end
   end
 
   def create
-    # binding.pry
     @post = Post.new(post_params)
     if @post.save
       redirect_to root_path
     else
       @category_parent_array = ["---"]
-      #データベースから、親カテゴリーのみ抽出し、配列化
       Category.where(ancestry: nil).each do |parent|
         @category_parent_array << parent.name
       end
@@ -34,15 +30,11 @@ class PostsController < ApplicationController
     @post = Post.includes(:images).find(params[:id])
   end
 
-  # 親カテゴリーが選択された後に動くアクション
   def get_category_children
-    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
     @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
   end
 
-  # 子カテゴリーが選択された後に動くアクション
   def get_category_grandchildren
-    #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
     @category_grandchildren = Category.find("#{params[:child_id]}").children
   end
 
