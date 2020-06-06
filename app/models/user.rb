2e6b2c478de811c440c3be694106d2baf23ceb61
takalivian <61145524+takalivian@users.noreply.github.com>
2020-06-06 22:58:58 +0900

    DMassociation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverse_of_relationships, source: :user
+  has_many :messages, dependent: :destroy
+  has_many :entries, dependent: :destroy
 
   mount_uploader :image, ImageUploader
 
