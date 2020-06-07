76ca2bbc76d8cc0bef0193dab71513afb9f7f382
takalivian <61145524+takalivian@users.noreply.github.com>
2020-06-07 13:19:29 +0900

    room-index

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,17 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
+  
+  def index
+    @currentEntries = current_user.entries
+    myRoomIds = []
+  
+    @currentEntries.each do |entry|
+      myRoomIds << entry.room.id
+    end
+  
+    @anotherEntries = Entry.where(room_id: myRoomIds).where('user_id != ?',current_user)
+  end
+
   def create
     @room = Room.create
     @entry1 = Entry.create(:room_id => @room.id, :user_id => current_user.id)
