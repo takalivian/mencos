cc3438023719d8aa28943591338d2f03a4fa5960
takalivian <61145524+takalivian@users.noreply.github.com>
2020-06-09 13:41:08 +0900

    ドロップダウンメニュー（子）

--- app/assets/javascripts/menue.js
+++ app/assets/javascripts/menue.js
@@ -3,3 +3,25 @@ $(function () {
     $('body').toggleClass('open');
   });
 });
+
+$(function(){
+  $('.dropdwn li').hover(function(){
+      $("ul:not(:animated)", this).slideDown();
+  }, function(){
+      $("ul.dropdwn_menu",this).slideUp();
+  });
+});
+
+$(function() {
+  $(".toppage__footer-nav-category").hover(function() {
+  $("ul.category1").toggle();
+  });
+  $(".toppage__footer-nav-category li ul").hide();
+  $(".toppage__footer-nav-category li").hover(function() {
+      $(">ul:not(:animated)", this).stop(true, true).slideDown("fast");
+      $(">a", this).addClass("active");
+  }, function() {
+      $(">ul:not(:animated)", this).stop(true, true).slideUp("fast");
+      $(">a", this).removeClass("active");
+});
+});
