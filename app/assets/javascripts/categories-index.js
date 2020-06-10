793c2e89a7e1da1982a998f440642eb01ba74404
takalivian <61145524+takalivian@users.noreply.github.com>
2020-06-10 18:20:16 +0900

    categories-indexリファクタリング

--- app/assets/javascripts/categories-index.js
+++ app/assets/javascripts/categories-index.js
@@ -1,51 +1,5 @@
-
-// IDに数字付与
 $(function(){
-  $('div.categories-wrapper__contents__main__contents__title').each(function(i){
-      $(this).attr('id','hide-text' + (i+1));
+  $('div.categories-wrapper__contents__main__contents__title').on('click', function() {
+    $(this).next('div.categories-wrapper__contents__main__contents__category').toggle('slow');
   });
 });
-
-$(function(){
-  $('div.categories-wrapper__contents__main__contents__category').each(function(i){
-      $(this).attr('id','text' + (i+1));
-  });
-});
-
-// 子表示
-$(function () {
-  $('#hide-text1').on('click', () => {
-      $('#text1').toggle('slow');
-  });
-});
-
-$(function () {
-  $('#hide-text2').on('click', () => {
-      $('#text2').toggle('slow');
-  });
-});
-
-$(function () {
-  $('#hide-text3').on('click', () => {
-      $('#text3').toggle('slow');
-  });
-});
-
-$(function () {
-  $('#hide-text4').on('click', () => {
-      $('#text4').toggle('slow');
-  });
-});
-
-$(function () {
-  $('#hide-text5').on('click', () => {
-      $('#text5').toggle('slow');
-  });
-});
-
-$(function () {
-  $('#hide-text6').on('click', () => {
-      $('#text6').toggle('slow');
-  });
-});
-
