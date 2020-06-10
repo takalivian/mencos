5241cd37f95b279e0377fd3b6aa3e4bb870e16ea
takalivian <61145524+takalivian@users.noreply.github.com>
2020-06-10 19:01:45 +0900

    コメントアウト削除

--- app/assets/javascripts/loading.js
+++ app/assets/javascripts/loading.js
@@ -1,13 +1,9 @@
 $(function(){
   $(function(){
     var loader = $('.loader-wrap');
-
-    //ページの読み込みが完了したらアニメーションを非表示
     $(window).on('load',function(){
       loader.fadeOut();
     });
-
-    //ページの読み込みが完了してなくても2秒後にアニメーションを非表示にする
     setTimeout(function(){
       loader.fadeOut();
     },2000);
