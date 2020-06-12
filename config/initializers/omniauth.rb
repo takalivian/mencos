068d78e64d4063aee8dfbdbaa9c669c5ed2d63b9
takalivian <61145524+takalivian@users.noreply.github.com>
2020-06-12 20:44:06 +0900

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :google_oauth2,
-#   Rails.application.credentials.google_oauth2[:key],
-#   Rails.application.credentials.google_oauth2[:secret]
-# end
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2,
+  Rails.application.credentials.google_oauth2[:key],
+  Rails.application.credentials.google_oauth2[:secret]
+end
