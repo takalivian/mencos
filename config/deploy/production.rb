00a66f4e043a12a79b6a56a55ee8d28c8a627cd7
takalivian <61145524+takalivian@users.noreply.github.com>
2020-09-25 16:45:49 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '3.113.80.162', user: 'ec2-user', roles: %w{app db web}
+server '54.248.6.26', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
